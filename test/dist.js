3c1858e661abf3da4ca5f37f0c960ba4b7823d90
synesenom <enys.mones@gmail.com>
2017-05-01 14:14:20 +0200

    some fixes

--- test/dist.js
+++ test/dist.js
@@ -97,8 +97,8 @@ describe('dice', function() {
         describe('uniform(min, max, n)', function () {
             it('should return an array of uniformly distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var xmin = Math.random()*100 - 50;
-                    var xmax = xmin + Math.random()*50;
+                    const xmin = Math.random()*100 - 50;
+                    const xmax = xmin + Math.random()*50;
                     ks_test(dist.uniform(xmin, xmax, LAPS), function (x) {
                         return (x-xmin) / (xmax-xmin);
                     });
@@ -109,7 +109,7 @@ describe('dice', function() {
         describe('exponential(lambda, n)', function () {
             it('should return an array of exponentially distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var lambda = Math.random()*10 + 1;
+                    const lambda = Math.random()*10 + 1;
                     ks_test(dist.exponential(lambda, LAPS), function (x) {
                         return 1 - Math.exp(-lambda * x);
                     });
@@ -120,8 +120,8 @@ describe('dice', function() {
         describe('pareto(xmin, alpha, n)', function () {
             it('should return an array of Pareto distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var xmin = Math.random()*10 + 1;
-                    var alpha = Math.random()*5 + 1;
+                    const xmin = Math.random()*10 + 1;
+                    const alpha = Math.random()*5 + 1;
                     ks_test(dist.pareto(xmin, alpha, LAPS), function (x) {
                         return 1 - Math.pow(xmin / x, alpha);
                     });
@@ -132,9 +132,9 @@ describe('dice', function() {
         describe('boundedPareto(xmin, xmax, alpha, n)', function () {
             it('should return an array of bounded Pareto distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var xmin = Math.random()*10 + 1;
-                    var xmax = xmin + Math.random()*20 + 2;
-                    var alpha = Math.random()*5 + 1;
+                    const xmin = Math.random()*10 + 1;
+                    const xmax = xmin + Math.random()*20 + 2;
+                    const alpha = Math.random()*5 + 1;
                     ks_test(dist.boundedPareto(xmin, xmax, alpha, LAPS), function (x) {
                         return (1 - Math.pow(xmin / x, alpha)) / (1 - Math.pow(xmin / xmax, alpha));
                     });
@@ -145,8 +145,8 @@ describe('dice', function() {
         describe('normal(mu, sigma, n)', function () {
             it('should return an array of normally distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var mu = Math.random()*10 + 1;
-                    var sigma = Math.random()*10 + 1;
+                    const mu = Math.random()*10 + 1;
+                    const sigma = Math.random()*10 + 1;
                     ks_test(dist.normal(mu, sigma, LAPS), function (x) {
                         return 0.5 * (1 + math.erf((x-mu)/(sigma*Math.sqrt(2))));
                     });
@@ -157,8 +157,8 @@ describe('dice', function() {
         describe('lognormal(mu, sigma, n)', function () {
             it('should return an array of log-normally distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var mu = Math.random()*10 + 1;
-                    var sigma = Math.random()*10 + 1;
+                    const mu = Math.random()*10 + 1;
+                    const sigma = Math.random()*10 + 1;
                     ks_test(dist.lognormal(mu, sigma, LAPS), function (x) {
                         return 0.5 * (1 + math.erf((Math.log(x)-mu)/(sigma*Math.sqrt(2))));
                     });
@@ -169,8 +169,8 @@ describe('dice', function() {
         describe('weibull(mu, sigma, n)', function () {
             it('should return an array of Weibull distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var lambda = Math.random()*10 + 0.1;
-                    var k = Math.random()*10 + 0.1;
+                    const lambda = Math.random()*10 + 0.1;
+                    const k = Math.random()*10 + 0.1;
                     ks_test(dist.weibull(lambda, k, LAPS), function (x) {
                         return 1 - Math.exp(-Math.pow(x/lambda, k));
                     });
@@ -182,7 +182,7 @@ describe('dice', function() {
         describe('poisson(mu, sigma, n)', function () {
             it('should return an array of Poisson distributed values', function () {
                 for (var t=0; t<TRIALS; t++) {
-                    var lambda = Math.random()*10 + 1;
+                    const lambda = Math.random()*10 + 1;
                     chi_test(dist.poisson(lambda, LAPS), function (x) {
                         return Math.pow(lambda, x) * Math.exp(-lambda) / math.factorial(x);
                     }, 1);
